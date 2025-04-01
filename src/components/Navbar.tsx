33eb96a6044b023495ca7fbf84893013ff09f6e3
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-04-01 11:50:42 -0400

    Add feedback

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ interface NavbarProps {
 }
 
 const Navbar: FC<NavbarProps> = ({ handleLogout }) => (
-  <div className="flex items-center w-screen justify-between p-4 sm:pr-10">
+  <div className="flex items-center w-screen justify-between sm:p-6 sm:py-8 p-4 py-5 sm:pr-10">
     <div
       className="text-3xl sm:text-4xl font-bold font-['Knewave'] tracking-wide cursor-pointer"
       onClick={() => (window.location.href = "/")}
@@ -20,6 +20,14 @@ const Navbar: FC<NavbarProps> = ({ handleLogout }) => (
       >
         About
       </Link>
+      <a
+        href="https://forms.gle/Pqmeex4c6ubKRDAh8"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full hidden sm:block"
+      >
+        Feedback
+      </a>
       <button
         onClick={handleLogout}
         className="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full text-red-600 hover:text-red-800"
